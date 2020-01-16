ee1c96a63efab83d860da2cf2ea4795d9b02c885
yitian1808@163.com <tangyansheng8316>
2020-01-16 15:45:29 +0800

    5555

--- src/main/java/cn/stylefeng/guns/config/web/ShiroConfig.java
+++ src/main/java/cn/stylefeng/guns/config/web/ShiroConfig.java
@@ -180,6 +180,7 @@ public class ShiroConfig {
         hashMap.put("/login", "anon");
         hashMap.put("/global/sessionError", "anon");
         hashMap.put("/kaptcha", "anon");
+        hashMap.put("/creditinfo", "anon");
         hashMap.put("/**", "user");
         shiroFilter.setFilterChainDefinitionMap(hashMap);
         return shiroFilter;
